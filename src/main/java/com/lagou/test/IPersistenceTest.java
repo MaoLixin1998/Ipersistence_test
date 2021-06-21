f16802514f36276b0373c73bc3a7e7a9eb023a71
MaoLixin1998 <maoxin98116@163.com>
2021-06-21 09:44:06 +0800

    自定义持久层框架，测试类

--- src/main/java/com/lagou/test/IPersistenceTest.java
+++ src/main/java/com/lagou/test/IPersistenceTest.java
@@ -1,4 +1,34 @@
 package com.lagou.test;
 
+import  com.lagou.io.Resources;
+import com.lagou.pojo.User;
+import com.lagou.sqlSession.SqlSession;
+import com.lagou.sqlSession.SqlSessionFactory;
+import com.lagou.sqlSession.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
 public class IPersistenceTest {
+    @Test
+    public void test() throws Exception {
+
+        InputStream resourceAsSteam = Resources.getResourceAsSteam("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsSteam);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //调用
+        User user = new User();
+        user.setId(1);
+        user.setUsername("aaa");
+
+
+        List<User> user2 = sqlSession.selectList("user.selectList");
+        for (User user1 : user2) {
+
+            System.out.println(user1);
+        }
+
+    }
 }
